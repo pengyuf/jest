3f4945e909b036954ea809550d53985bc9bd3218
best <2907647445@qq.com>
2024-04-17 00:26:15 +0800

    测试异步

--- sum.test.js
+++ sum.test.js
@@ -9,3 +9,63 @@ test("object assignment", () => {
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
+
+function fetchData(willFail = false) {
+  return new Promise((resolve, reject) => {
+    if (willFail) {
+      // 如果条件满足，则拒绝 Promise 并抛出一个错误
+      reject("error");
+    } else {
+      // 如果条件不满足，则解决 Promise 并返回一些数据
+      resolve("peanut butter");
+    }
+  });
+}
+
+test("测试异步1", () => {
+  return fetchData(false).then((data) => {
+    expect(data).toBe("peanut butter");
+  });
+});
+
+test("测试异步2", async () => {
+  const data = await fetchData(false);
+  expect(data).toBe("peanut butter");
+});
+
+test("测试异步是否抛出错误", async () => {
+  expect.assertions(1);
+  try {
+    await fetchData(true);
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
+});
+
+function fetchDataCallback(callback) {
+  // 模拟异步操作，比如网络请求
+  setTimeout(() => {
+    // 假设这是从某个源获取的数据
+    const data = "peanut butter";
+
+    // 没有错误，所以传递 null 作为错误对象
+    callback(null, data);
+  }, 1000); // 延迟1秒以模拟异步行为
+}
+
+test("the data is peanut butter", (done) => {
+  function callback(error, data) {
+    if (error) {
+      done(error);
+      return;
+    }
+    try {
+      expect(data).toBe("peanut butter");
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+
+  fetchDataCallback(callback);
+});
